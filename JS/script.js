539702e700bbf4f478c45ad1b530dd9ee666523f
ArthurShakirov125 <@rbiu.local>
2021-10-08 17:24:19 +0500

    js work

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,17 @@
 const btnCreateList = document.querySelector("#btn-create-list");
 const desk = document.querySelector("#desk");
-
+let counter = 1;
 btnCreateList.addEventListener("click",function (){
     let list = document.createElement("div");
-    let listName = document.getElementById("list-name").nodeValue;
-    list.innerHTML = listName;
+    list.classList.add("list");
+    let listName = document.getElementById("list-name").value;
+    let h2 = document.createElement("H2");
+    if(listName = " ")
+    {
+        listName = "New list " + counter
+    }
+    h2.innerText = listName;
+    list.append(h2);
     desk.append(list);
+    counter++;
 })
