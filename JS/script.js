07fae1f08452a72abd4476adf09d20281b3a5ada
Shakirov_Arthur <Wolverane32@gmail.com>
2021-10-12 14:28:01 +0500

    Закончил работу

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,12 @@
 const btnCreateList = document.querySelector("#btn-create-list");
 const desk = document.querySelector("#desk");
 let counter = 1;
+const clearBtn = document.querySelector("#clear_desk");
+clearBtn.addEventListener("click",function(){
+    desk.innerHTML = null;
+    counter = 1;
+})
+
 btnCreateList.addEventListener("click",function (){
     let list = document.createElement("div");
     list.classList.add("list");
@@ -8,10 +14,16 @@ btnCreateList.addEventListener("click",function (){
     let h2 = document.createElement("H2");
     if(listName = " ")
     {
-        listName = "New list " + counter
+        listName = "New list " + counter;
+        counter++;
     }
-    h2.innerText = listName;
+    h2.innerHTML = listName;
     list.append(h2);
+    let img = document.createElement("img");
+    img.setAttribute("src","img/pen-solid.svg");
+    img.setAttribute("alt","redact list name");
+    img.setAttribute("width","30px");
+    list.append(img);
     desk.append(list);
-    counter++;
+    
 })
