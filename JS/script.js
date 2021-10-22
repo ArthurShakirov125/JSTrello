ade47e0b34a87ba008abf0d4d3807dfdf20b3262
Студенческая <@rbiu.local>
2021-10-22 17:23:52 +0500

    продолжаю работу

--- JS/script.js
+++ JS/script.js
@@ -2,28 +2,50 @@ const btnCreateList = document.querySelector("#btn-create-list");
 const desk = document.querySelector("#desk");
 let counter = 1;
 const clearBtn = document.querySelector("#clear_desk");
-clearBtn.addEventListener("click",function(){
-    desk.innerHTML = null;
-    counter = 1;
-})
+const inputListName = document.querySelector("#list-name");
 
-btnCreateList.addEventListener("click",function (){
+function EditList(e){
+    if(e.target.classList.contains("edit-list")){
+        let list = e.target.firstElementChild();
+        let h2 = list.firstElementChild();
+        h2.setAttribute("contenteditable", "true");
+        h2.focus();
+    }
+}
+function AddList() {
     let list = document.createElement("div");
     list.classList.add("list");
     let listName = document.getElementById("list-name").value;
     let h2 = document.createElement("H2");
-    if(listName = " ")
-    {
+    if (listName == "") {
         listName = "New list " + counter;
         counter++;
     }
     h2.innerHTML = listName;
     list.append(h2);
     let img = document.createElement("img");
-    img.setAttribute("src","img/pen-solid.svg");
-    img.setAttribute("alt","redact list name");
-    img.setAttribute("width","30px");
+    img.setAttribute("src", "img/pen-solid.svg");
+    img.setAttribute("alt", "redact list name");
+    img.setAttribute("width", "30px");
+    img.classList.add("edit-list");
     list.append(img);
     desk.append(list);
-    
+}
+
+inputListName.addEventListener("keydown", e => {
+    if(e.key == "Enter"){
+        btnCreateList.click();
+    }
+ }
+);
+
+clearBtn.addEventListener("click", function () {
+    desk.innerHTML = null;
+    counter = 1;
 })
+
+
+desk.addEventListener("click", EditList);
+btnCreateList.addEventListener("click", AddList);
+
+
